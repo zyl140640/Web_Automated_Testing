fbaf5af50388073deb2750d576e6b81d164cd45d
Jiang Yan <zyl140640@163.com>
2023-09-25 15:53:11 +0800

    调整Main文件

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-# test_search.py
 import os
 import time
 
 import pytest
 
-from common.tools import detect_video
-
 if __name__ == '__main__':
-    # 清空录屏文件
-    detect_video()
+    # # 清空录屏文件
+    # detect_video()
     # 执行pytest
     pytest.main(['-s', '-v', ])
     # pytest.main(['-s', '-v', 'testcases/test_add_gateway.py::TestAddGateway::test_add_gateway'])
