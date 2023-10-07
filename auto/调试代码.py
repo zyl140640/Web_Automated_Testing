13c1d8c86a29c8b642445c3755b1a93856efab8f
Jiang Yan <zyl140640@163.com>
2023-10-07 13:20:17 +0800

    修正

--- auto/调试代码.py
+++ auto/调试代码.py
@@ -7,22 +7,12 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    # page.goto("http://36.134.46.91:7070/login")
-    page.pause()
     bs = BasePage(page)
     bs.go_url("http://36.134.46.91:7070/login")
     ces = page.get_by_placeholder("请输入用户名")
     print(page.get_by_placeholder("请输入用户名"))
     print(type(page.get_by_placeholder("请输入用户名")))
     ces.click()
-
-
-
-
-
-
-
-
 
     bs.input_data(page.get_by_placeholder("请输入用户名"), "测试", "用户输入框内")
     bs.click(page.get_by_role("button", name="登 录"), "登录按钮")
