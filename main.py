6bc6e29dc30453faa2d8fc1bfb54daead74bf739
Jiang Yan <zyl140640@163.com>
2023-10-23 09:52:09 +0800

    注释allure名称修改信息

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+
 ''
 import pytest
 
@@ -9,12 +10,13 @@ if __name__ == '__main__':
     clear_directory()
     # 执行pytest
     pytest.main(['-s', '-v'])
-    # pytest.main(['-s', '-v', 'testcases/test_1_core/test_project.py::TestProject::test_add_project'])
+    # pytest.main(['-s', '-v',
+    #              'testcases/test_2_project/gateway_manage/test_gateway_pt.py::TestGatewayXieYi::test_read_gateway'])
     time.sleep(3)
     # 生成测试报告
     os.system("allure generate ./temps -o ./reports --clean")
-    # 修改Allure测试报告标题
-    set_windows_title("御控工业云平台Web自动化测试报告")
-    report_title = get_json_data("御控工业云平台Web自动化测试报告")
-    write_json_data(report_title)
-    # Jenkins上运行会导致出问题，找不到文件
+    # # 修改Allure测试报告标题
+    # set_windows_title("御控工业云平台Web自动化测试报告")
+    # report_title = get_json_data("御控工业云平台Web自动化测试报告")
+    # write_json_data(report_title)
+    # # Jenkins上运行会导致出问题，找不到文件
