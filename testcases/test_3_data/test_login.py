9f3ea23cc6ba923ae6c7aec2559dcbf0f7653c61
Jiang Yan <zyl140640@163.com>
2023-11-28 15:20:37 +0800

    跑账号

--- testcases/test_3_data/test_login.py
+++ testcases/test_3_data/test_login.py
@@ -11,7 +11,6 @@ yaml = login_yaml("auto/data.json")
 class TestMain:
 
     @pytest.mark.parametrize('args', yaml['rows'])
-    @allure.feature("登录")
     def test_mimu_api(self, page,args):
         bs_init = BasePage(page)
         bs_init.logger.info(
@@ -21,6 +20,7 @@ class TestMain:
             bs_init.logger.info("当前企业编码不为空，走当前账号企业登录页面")
             bs_init.logger.info(f"http://192.168.110.210:9016/login?id={args['Code']}")
             bs_init.go_url(f"http://192.168.110.210:9016/login?id={args['Code']}")
+            bs_init.wait_for_timeouts(3000)
             bs_init.input_data(page.get_by_placeholder("请输入用户名"), args['Code'], "输入账号信息")
             bs_init.input_data(page.get_by_placeholder("请输入登录密码"), "123456", "输入密码信息")
             drop_button = page.get_by_role("article").locator("form div").filter(
@@ -32,16 +32,14 @@ class TestMain:
             page.mouse.move(mov_x, box['y'] + box['height'] / 2)
             page.mouse.up()
             bs_init.click(page.get_by_role("button", name="登 录"), "登录按钮")
-            Dialog = page.get_by_text("快速接入平台指引说明").count()
-            if Dialog == 1:
-                bs_init.logger.info("----存在首页弹窗----")
-                page.get_by_label("Close", exact=True).click()
-            else:
-                bs_init.logger.info("----未存在弹窗直接跳过----")
+            page.get_by_text("快速接入平台指引说明").wait_for()
+            bs_init.logger.info("----存在首页弹窗----")
+            page.get_by_label("Close", exact=True).click()
         else:
             bs_init.logger.info("当前企业编码为空，走默认登录页面")
             bs_init.go_url("http://192.168.110.210:9016/")
-            bs_init.input_data(page.get_by_placeholder("请输入用户名"), args['Code'], "输入账号信息")
+            bs_init.wait_for_timeouts(3000)
+            bs_init.input_data(page.get_by_placeholder("请输入用户名"), args['UserNO'], "输入账号信息")
             bs_init.input_data(page.get_by_placeholder("请输入登录密码"), "123456", "输入密码信息")
             drop_button = page.get_by_role("article").locator("form div").filter(
                 has_text="请按住滑块拖动 登 录").locator("div").nth(4)
@@ -52,11 +50,7 @@ class TestMain:
             page.mouse.move(mov_x, box['y'] + box['height'] / 2)
             page.mouse.up()
             bs_init.click(page.get_by_role("button", name="登 录"), "登录按钮")
-            Dialog = page.get_by_text("快速接入平台指引说明").count()
-            if Dialog == 1:
-                bs_init.logger.info("----存在首页弹窗----")
-                page.get_by_label("Close", exact=True).click()
-            else:
-                bs_init.logger.info("----未存在弹窗直接跳过----")
+            page.get_by_text("快速接入平台指引说明").wait_for()
+            page.get_by_label("Close", exact=True).click()
 
 
